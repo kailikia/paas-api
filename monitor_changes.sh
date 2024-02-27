bc34591c5b7dce58e6e2c1ed70faaf3e5a646154
kailikia <edwinkailikia@gmail.com>
2024-02-27 23:18:00 +0300

    setup html deploy 7

---
+++ monitor_changes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Commands to execute when changes occur
+# Example: echo "Changes detected!"
+
+# Log the changes to a file
+echo "Changes detected at $(date)" >> /var/www/monitor_logfile.log
