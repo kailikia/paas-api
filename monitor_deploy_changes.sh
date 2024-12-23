c44014013c49bf6005b7223de79a47cec5008e78
kailikia <edwinkailikia@gmail.com>
2024-12-23 18:53:04 +0300

    db added

--- monitor_deploy_changes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Commands to execute when changes occur
-# Example: echo "Changes detected!"
-
-# Log the changes to a file
-echo "Changes detected at $(date)" >> /var/www/paas/monitor_logfile.log
-# echo "Changes detected at $(date)" >> /var/www/monitorneeew_logfile.log
