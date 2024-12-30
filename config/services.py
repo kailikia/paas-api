6502641fef050535be46dc6dbae4dc99118d8a84
fridah51 <fridahshiku1@gmail.com>
2024-12-30 22:10:53 +0300

    paths 1

--- config/services.py
+++ config/services.py
@@ -20,21 +20,38 @@ client = Client(DOTOKEN)
 def get_subdomain_logs(subdomain):
     try:
         cur_path = os.path.join(os.getcwd(), 'deployed_apps')
+
         if os.path.exists(cur_path):
-            # os.chdir(cur_path)
 
-            deploy_subdomain_logs = os.path.join("../deployed_apps_logs", subdomain +".json")
-            print("Subdomain Logs----------------", deploy_subdomain_logs)
+            if os.getcwd() == cur_path:
 
-            #Check if the file exists
-            if os.path.exists(deploy_subdomain_logs):
-                # Open and read the file content
-                with open(deploy_subdomain_logs, 'r') as file:
-                    file_content = file.read()
-                    return json.loads(file_content)  # Assuming the file contains JSON data
+                deploy_subdomain_logs = os.path.join("../deployed_apps_logs", subdomain +".json")
+                print("Subdomain Logs 1----------------", deploy_subdomain_logs)
+
+                #Check if the file exists
+                if os.path.exists(deploy_subdomain_logs):
+                    # Open and read the file content
+                    with open(deploy_subdomain_logs, 'r') as file:
+                        file_content = file.read()
+                        return json.loads(file_content)  # Assuming the file contains JSON data
+                else:
+                    return {"Error": "Log file does not exist."}
+                return deploy_subdomain_logs
             else:
-                return {"Error": "Log file does not exist."}
-            return deploy_subdomain_logs
+                os.chdir(cur_path)
+
+                deploy_subdomain_logs = os.path.join("../deployed_apps_logs", subdomain +".json")
+                print("Subdomain Logs 2----------------", deploy_subdomain_logs)
+
+                #Check if the file exists
+                if os.path.exists(deploy_subdomain_logs):
+                    # Open and read the file content
+                    with open(deploy_subdomain_logs, 'r') as file:
+                        file_content = file.read()
+                        return json.loads(file_content)  # Assuming the file contains JSON data
+                else:
+                    return {"Error": "Log file does not exist."}
+
         else:
             return {"Error": "Deployed app path doesn't exist." + cur_path }
     except Exception as e:
