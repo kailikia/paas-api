789d895a5d2e1744fb8c006ba83d086bacd5c608
fridah51 <fridahshiku1@gmail.com>
2024-12-30 22:20:40 +0300

    paths 3

--- config/services.py
+++ config/services.py
@@ -19,7 +19,7 @@ client = Client(DOTOKEN)
 
 def get_subdomain_logs(subdomain):
     try:
-        cur_path = os.path.join("/deployed_apps")
+        cur_path = os.path.join(os.getcwd(),"deployed_apps")
         os.chdir(cur_path)
 
         if os.path.exists(cur_path):
