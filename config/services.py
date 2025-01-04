48fdd45737bd1e91fe7d5315eb43cb6400a1332f
fridah51 <fridahshiku1@gmail.com>
2025-01-04 12:00:20 +0300

    open sesame

--- config/services.py
+++ config/services.py
@@ -246,11 +246,11 @@ def deploy_html_by_ssh_subprocess(github_url, subdomain, user):
         os.chdir(folder_path)
 
         # Create the success report file
-        file_path = os.path.join(folder_path, subdomain+".sh")
-        with open(file_path, "w") as file:
+        success_file = os.path.join("/success-report", subdomain +".sh")
+        with open(success_file, "a") as file:
             file.write("run " +subdomain+" app on port " +str(file_count)+"")
 
-        print(f"Success report created: {file_path}")
+        print(f"Success report created: {success_file}")
 
     except OSError as e:
         print(f"Error creating success report for {subdomain}: {e}")
