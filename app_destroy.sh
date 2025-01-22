89c87405fe689935444ebad3e256452612e311a7
kailikia <edwinkailikia@gmail.com>
2025-01-22 03:05:15 +0300

    acme 16

--- app_destroy.sh
+++ app_destroy.sh
@@ -15,28 +15,28 @@ APP_NAME="techcamp.app"
 
 # Step 3: Delete files and folders
 echo "Step 3: Deleting files and folders..."
-rm -rf /root/.acme.sh/${subdomain}.${APP_NAME}_ecc
+sudo sudo rm -rf /root/.acme.sh/${subdomain}.${APP_NAME}_ecc
 echo "Deleted: /root/.acme.sh/${subdomain}.${APP_NAME}_ecc"
 
-rm -f /etc/nginx/sites-available/${subdomain}.${APP_NAME}
+sudo sudo rm -f /etc/nginx/sites-available/${subdomain}.${APP_NAME}
 echo "Deleted: /etc/nginx/sites-available/${subdomain}.${APP_NAME}"
 
-rm -f /etc/nginx/sites-enabled/${subdomain}.${APP_NAME}
+sudo rm -f /etc/nginx/sites-enabled/${subdomain}.${APP_NAME}
 echo "Deleted: /etc/nginx/sites-enabled/${subdomain}.${APP_NAME}"
 
-rm -rf /var/www/paas/deployed_apps/${subdomain}
+sudo rm -rf /var/www/paas/deployed_apps/${subdomain}
 echo "Deleted: /var/www/paas/deployed_apps/${subdomain}"
 
-rm -f /var/www/paas/deployed_nginx_files/${subdomain}.${APP_NAME}
+sudo rm -f /var/www/paas/deployed_nginx_files/${subdomain}.${APP_NAME}
 echo "Deleted: /var/www/paas/deployed_nginx_files/${subdomain}.${APP_NAME}"
 
-rm -rf /var/www/logs/${subdomain}
+sudo rm -rf /var/www/logs/${subdomain}
 echo "Deleted: /var/www/logs/${subdomain}"
 
-rm -f /var/www/paas/success-report/${subdomain}.sh
+sudo rm -f /var/www/paas/success-report/${subdomain}.sh
 echo "Deleted: /var/www/paas/success-report/${subdomain}.sh"
 
-rm -f /var/www/paas/deployed_app_logs/${subdomain}.json
+sudo rm -f /var/www/paas/deployed_app_logs/${subdomain}.json
 echo "Deleted: /var/www/paas/deployed_app_logs/${subdomain}.json"
 
 # Step 4: Delete record from the database
@@ -48,12 +48,12 @@ echo "Database record deletion (if any) completed for ${subdomain}."
 # Step 5: Remove Docker container
 echo "Step 5: Stopping and removing Docker container..."
 docker stop ${subdomain}-app
-docker rm ${subdomain}
+docker sudo rm ${subdomain}
 echo "Docker container stopped and removed: ${subdomain}"
 
 # Step 6: Delete Docker image
 echo "Step 6: Deleting Docker image..."
-docker rmi ${subdomain}
+docker sudo rmi ${subdomain}
 echo "Docker image deleted: ${subdomain}"
 
 # Final Step: Completion message
