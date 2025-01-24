26cb39356bd231dd2ee1ade2488cb19ad0af93b1
fridah51 <fridahshiku1@gmail.com>
2025-01-24 16:28:44 +0300

    webroot version1

--- mybuildscript.sh
+++ mybuildscript.sh
@@ -2,7 +2,7 @@ git pull
 docker-compose down
 sudo docker system prune -a -f
 docker-compose up --build -d
-sudo supervisorctl restart all
+# sudo supervisorctl restart all
 # acme.sh --config-home /root/.acme.sh --issue -d vedi.techcamp.app --standalone --cert-home /etc/nginx/ssl/vedi.techcamp.app
 # tail -f /var/log/supervisor/paas_supervisor_stdout.log
 # acme.sh --issue -d doman.techcamp.app --nginx
