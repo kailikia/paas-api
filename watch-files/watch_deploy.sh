8fcf13b11224c6f125aea5a78770e069efee1c96
fridah51 <fridahshiku1@gmail.com>
2025-02-14 16:51:58 +0300

    likere

--- watch-files/watch_deploy.sh
+++ watch-files/watch_deploy.sh
@@ -3,9 +3,8 @@
 WATCH_DIR="/var/www/paas/success-report"
 SCRIPT_TO_RUN="/var/www/paas-api/action-scripts/app_deployment.sh"
 
-while true; do
-    /usr/bin/inotifywait -e create "$WATCH_DIR" && /bin/bash "$SCRIPT_TO_RUN"
-    sleep 1  # Prevent high CPU usage
+/usr/bin/inotifywait -e create "$WATCH_DIR" && /bin/bash "$SCRIPT_TO_RUN"
+
 done
 
 
--- watch-files/watch_destroy.sh
+++ watch-files/watch_destroy.sh
@@ -4,6 +4,6 @@ WATCH_DIR="/var/www/paas/destroy-report"
 SCRIPT_TO_RUN="/var/www/paas-api/action-scripts/app_destroy.sh"
 
 while true; do
-    /usr/bin/inotifywait -e create "$WATCH_DIR" && /bin/bash "$SCRIPT_TO_RUN"
+    /usr/bin/inotifywait -e create "$WATCH_DIR"|/bin/bash "$SCRIPT_TO_RUN"
     sleep 1  # Prevent high CPU usage
 done
