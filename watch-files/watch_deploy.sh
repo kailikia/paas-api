c2f6a06f0d2502e64e835ffac11513a11a1f1a3b
fridah51 <fridahshiku1@gmail.com>
2025-02-22 21:28:46 +0300

    oh nana 9.28

--- watch-files/watch_deploy.sh
+++ watch-files/watch_deploy.sh
@@ -7,13 +7,7 @@ LOG_FILE="/var/log/supervisor/paas_supervisor_stdout.log"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Watching for changes in $WATCH_DIR" >> "$LOG_FILE"
 
-# if pgrep -f inotifywait > /dev/null; then
-#     echo "Inotify is already running. Exiting." >> "$LOG_FILE"
-#     exit 0
-# fi
-
-
-/usr/bin/inotifywait -e create /var/www/paas/success-report | /bin/bash /var/www/paas-api/action-scripts/app_deployment.sh >> "$LOG_FILE" 2>&1 
+exec /usr/bin/inotifywait -e create /var/www/paas/success-report | /bin/bash /var/www/paas-api/action-scripts/app_deployment.sh >> "$LOG_FILE" 2>&1 
 
 # /bin/bash -c "/usr/bin/inotifywait -e create /var/www/paas/success-report | /bin/bash /var/www/paas-api/action-scripts/app_deployment.sh" >> "$LOG_FILE" 2>&1 
 
